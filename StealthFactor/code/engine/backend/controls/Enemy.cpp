KoltesDigital/Game-Architecture-Epita-2018

Split gameplay in backend and frontend + Remove physics

StealthFactor/code/engine/backend/controls/Enemy.cpp
@@ -1,0 +1,55 @@
+#include "Enemy.hpp"
+
+#include <engine/assets/AssetsManager.hpp>
+#include <engine/backend/Character.hpp>
+#include <engine/backend/ControlContext.hpp>
+#include <engine/backend/ControlListener.hpp>
+#include <engine/backend/MapDescriptor.hpp>
+
+namespace engine
+{
+	namespace backend
+	{
+		namespace controls
+		{
+			Enemy::Enemy(Character &character, ControlContext &context, const EnemyDescriptor &descriptor, Character &playerCharacter)
+				: Control{ character, context }
+				, _playerCharacter{ playerCharacter }
+			{
+				_archetype = context.assetsManager.getAsset<Archetype>(descriptor.archetypeName);
+			}
+
+			void Enemy::update()
+			{
+				auto &playerPosition = _playerCharacter.position;
+				auto &myPosition = getCharacter().position;
+
+				auto offsetRow = myPosition.row - playerPosition.row;
+				auto offsetColumn = myPosition.column - playerPosition.column;
+				int distance2 = offsetRow * offsetRow + offsetColumn * offsetColumn;
+				if (distance2 <= _archetype->visionRadius * _archetype->visionRadius)
+				{
+					if (_shootDelayCounter < _archetype->shootDelay)
+					{
+						++_shootDelayCounter;
+					}
+					else
+					{
+						getContext().controlListener.gameOver();
+					}
+				}
+				else
+				{
+					_shootDelayCounter = 0;
+				}
+
+				getContext().controlListener.characterSkipped(getCharacter());
+			}
+
+			std::string Enemy::getPrefabName() const
+			{
+				return _archetype->prefabName;
+			}
+		}
+	}
+}

StealthFactor/code/engine/frontend/Prefab.cpp
@@ -1,0 +1,116 @@
+#include "Prefab.hpp"
+
+#include <cassert>
+#include <engine/assets/AssetsManager.hpp>
+#include <engine/frontend/Entity.hpp>
+#include <engine/frontend/EntityContext.hpp>
+#include <engine/frontend/components/Camera.hpp>
+#include <engine/frontend/components/Renderer.hpp>
+#include <engine/frontend/components/Transform.hpp>
+#include <engine/serialization/XMLLoader.hpp>
+
+namespace engine
+{
+	namespace frontend
+	{
+		struct PrefabComponent
+		{
+			virtual void instantiate(Entity &, EntityContext &) const
+			{
+				assert(false);
+			}
+		};
+
+		namespace prefabComponents
+		{
+			struct Camera : public PrefabComponent
+			{
+				void instantiate(Entity &entity, EntityContext &) const override
+				{
+					entity.addComponent<components::Camera>();
+				}
+			};
+
+			struct Renderer : public PrefabComponent
+			{
+				std::string shapelistName;
+
+				void instantiate(Entity &entity, EntityContext &) const override
+				{
+					auto &renderer = entity.addComponent<components::Renderer>();
+					renderer.setShapeListName(shapelistName);
+				}
+			};
+		}
+
+		Prefab::Prefab()
+		{
+		}
+
+		Prefab::~Prefab()
+		{
+		}
+
+		std::unique_ptr<Entity> Prefab::instantiate(EntityContext &context) const
+		{
+			auto entity{ std::make_unique<Entity>(context) };
+
+			entity->addComponent<components::Transform>();
+
+			for (auto &component : components)
+			{
+				component->instantiate(*entity, context);
+			}
+
+			return entity;
+		}
+	}
+
+	namespace assets
+	{
+		template <>
+		struct AssetTypeRegistry<frontend::Prefab>
+		{
+			static serialization::TypeRegistry value;
+
+			static struct Init
+			{
+				Init()
+				{
+					value.registerClass<frontend::prefabComponents::Camera>("camera");
+					value.registerClass<frontend::prefabComponents::Renderer>("renderer");
+				}
+			} init;
+		};
+
+		serialization::TypeRegistry AssetTypeRegistry<frontend::Prefab>::value;
+		AssetTypeRegistry<frontend::Prefab>::Init AssetTypeRegistry<frontend::Prefab>::init;
+	}
+
+	namespace serialization
+	{
+		template <>
+		void serialize<frontend::PrefabComponent>(frontend::PrefabComponent &, serialization::Serializer &)
+		{
+		}
+
+		template <>
+		void serialize<frontend::prefabComponents::Camera>(frontend::prefabComponents::Camera &camera, serialization::Serializer &serializer)
+		{
+			serialize(static_cast<frontend::PrefabComponent &>(camera), serializer);
+		}
+
+		template <>
+		void serialize<frontend::prefabComponents::Renderer>(frontend::prefabComponents::Renderer &renderer, serialization::Serializer &serializer)
+		{
+			serialize(static_cast<frontend::PrefabComponent &>(renderer), serializer);
+			serializer.declare("shapelist", renderer.shapelistName);
+		}
+
+		template <>
+		void serialize<frontend::Prefab>(frontend::Prefab &prefab, serialization::Serializer &serializer)
+		{
+			serializer.declare("components", prefab.components);
+		}
+	}
+}

KoltesDigital/Game-Architecture-Epita-2019

StealthFactor/code/engine/backend/controls/Enemy.cpp
@@ -1,0 +1,55 @@
+#include "Enemy.hpp"
+
+#include <engine/assets/AssetManager.hpp>
+#include <engine/backend/Character.hpp>
+#include <engine/backend/ControlContext.hpp>
+#include <engine/backend/ControlListener.hpp>
+#include <engine/backend/MapDescriptor.hpp>
+
+namespace engine
+{
+	namespace backend
+	{
+		namespace controls
+		{
+			Enemy::Enemy(Character &character, ControlContext &context, const EnemyDescriptor &descriptor, Character &playerCharacter)
+				: Control{ character, context }
+				, _playerCharacter{ playerCharacter }
+			{
+				_archetype = context.assetsManager.getAsset<Archetype>(descriptor.archetypeName);
+			}
+
+			void Enemy::update()
+			{
+				auto &playerPosition = _playerCharacter.position;
+				auto &myPosition = getCharacter().position;
+
+				auto offsetRow = myPosition.row - playerPosition.row;
+				auto offsetColumn = myPosition.column - playerPosition.column;
+				int distance2 = offsetRow * offsetRow + offsetColumn * offsetColumn;
+				if (distance2 <= _archetype->visionRadius * _archetype->visionRadius)
+				{
+					if (_shootDelayCounter < _archetype->shootDelay)
+					{
+						++_shootDelayCounter;
+					}
+					else
+					{
+						getContext().controlListener.gameOver();
+					}
+				}
+				else
+				{
+					_shootDelayCounter = 0;
+				}
+
+				getContext().controlListener.characterSkipped(getCharacter());
+			}
+
+			std::string Enemy::getPrefabName() const
+			{
+				return _archetype->prefabName;
+			}
+		}
+	}
+}

StealthFactor/code/engine/frontend/Prefab.cpp
@@ -1,0 +1,116 @@
+#include "Prefab.hpp"
+
+#include <cassert>
+#include <engine/assets/AssetManager.hpp>
+#include <engine/frontend/Entity.hpp>
+#include <engine/frontend/EntityContext.hpp>
+#include <engine/frontend/components/Camera.hpp>
+#include <engine/frontend/components/Renderer.hpp>
+#include <engine/frontend/components/Transform.hpp>
+#include <engine/serialization/XMLLoader.hpp>
+
+namespace engine
+{
+	namespace frontend
+	{
+		struct PrefabComponent
+		{
+			virtual void instantiate(Entity &, EntityContext &) const
+			{
+				assert(false);
+			}
+		};
+
+		namespace prefabComponents
+		{
+			struct Camera : public PrefabComponent
+			{
+				void instantiate(Entity &entity, EntityContext &) const override
+				{
+					entity.addComponent<components::Camera>();
+				}
+			};
+
+			struct Renderer : public PrefabComponent
+			{
+				std::string shapelistName;
+
+				void instantiate(Entity &entity, EntityContext &) const override
+				{
+					auto &renderer = entity.addComponent<components::Renderer>();
+					renderer.setShapeListName(shapelistName);
+				}
+			};
+		}
+
+		Prefab::Prefab()
+		{
+		}
+
+		Prefab::~Prefab()
+		{
+		}
+
+		std::unique_ptr<Entity> Prefab::instantiate(EntityContext &context) const
+		{
+			std::unique_ptr<Entity> entity{ new Entity{ context } };
+
+			entity->addComponent<components::Transform>();
+
+			for (auto &component : components)
+			{
+				component->instantiate(*entity, context);
+			}
+
+			return entity;
+		}
+	}
+
+	namespace assets
+	{
+		template <>
+		struct AssetTypeRegistry<frontend::Prefab>
+		{
+			static serialization::TypeRegistry value;
+
+			static struct Init
+			{
+				Init()
+				{
+					value.registerClass<frontend::prefabComponents::Camera>("camera");
+					value.registerClass<frontend::prefabComponents::Renderer>("renderer");
+				}
+			} init;
+		};
+
+		serialization::TypeRegistry AssetTypeRegistry<frontend::Prefab>::value{};
+		AssetTypeRegistry<frontend::Prefab>::Init AssetTypeRegistry<frontend::Prefab>::init;
+	}
+
+	namespace serialization
+	{
+		template <>
+		void serialize<frontend::PrefabComponent>(frontend::PrefabComponent &, serialization::Serializer &)
+		{
+		}
+
+		template <>
+		void serialize<frontend::prefabComponents::Camera>(frontend::prefabComponents::Camera &camera, serialization::Serializer &serializer)
+		{
+			serialize(static_cast<frontend::PrefabComponent &>(camera), serializer);
+		}
+
+		template <>
+		void serialize<frontend::prefabComponents::Renderer>(frontend::prefabComponents::Renderer &renderer, serialization::Serializer &serializer)
+		{
+			serialize(static_cast<frontend::PrefabComponent &>(renderer), serializer);
+			serializer.declare("shapelist", renderer.shapelistName);
+		}
+
+		template <>
+		void serialize<frontend::Prefab>(frontend::Prefab &prefab, serialization::Serializer &serializer)
+		{
+			serializer.declare("components", prefab.components);
+		}
+	}
+}